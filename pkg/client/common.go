77e8cf899f2635bb11d134bf85c4697718f9c2c5
Gigi Sayfan <the.gigi@gmail.com>
2022-12-17 03:05:46 -0800

    even better way to load kube config + optionally set context

--- pkg/client/common.go
+++ pkg/client/common.go
@@ -1,45 +1,10 @@
 package client
 
 import (
-    "errors"
-    yaml "gopkg.in/yaml.v3"
-    "io/ioutil"
     "k8s.io/client-go/rest"
     "k8s.io/client-go/tools/clientcmd"
+    clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 )
-
-type Cluster struct {
-    Cluster map[string]string
-    Name    string
-}
-
-type KubeConfig struct {
-    ApiVersion string
-    Clusters   []Cluster
-    Contexts   []interface{}
-}
-
-func getServerUrl(kubeconfigPath string, kubeContext string) (url string, err error) {
-    data, err := ioutil.ReadFile(kubeconfigPath)
-    if err != nil {
-        return
-    }
-    var config KubeConfig
-    err = yaml.Unmarshal(data, &config)
-    if err != nil {
-        return
-    }
-
-    for _, cluster := range config.Clusters {
-        if cluster.Name == kubeContext {
-            url = cluster.Cluster["server"]
-            return
-        }
-    }
-
-    err = errors.New("No such context: " + kubeContext)
-    return
-}
 
 func getKubeConfig(kubeconfigPath string, kubeContext string) (config *rest.Config, err error) {
     if kubeconfigPath == "" {
@@ -47,21 +12,17 @@ func getKubeConfig(kubeconfigPath string, kubeContext string) (config *rest.Conf
         return
     }
 
-    // If kubeContxt is provided fetch the server URL from the the kube config file
-    var serverUrl string
+    var conf *clientcmdapi.Config
+    conf, err = clientcmd.LoadFromFile(kubeconfigPath)
+    if err != nil {
+        return
+    }
+
+    overrides := &clientcmd.ConfigOverrides{}
     if kubeContext != "" {
-        serverUrl, err = getServerUrl(kubeconfigPath, kubeContext)
-        if err != nil {
-            return
-        }
+        overrides.CurrentContext = kubeContext
     }
 
-    // If we have a server URL we don't need the kubeConfig file anymore
-    if serverUrl != "" {
-        kubeconfigPath = ""
-    }
-
-    // when calling BuildConfigFromFlags we will have either server URL or kube config file
-    config, err = clientcmd.BuildConfigFromFlags(serverUrl, kubeconfigPath)
+    config, err = clientcmd.NewDefaultClientConfig(*conf, overrides).ClientConfig()
     return
 }
