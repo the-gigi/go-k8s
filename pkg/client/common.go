11954c3a5046844e1584993d16bb0988741c3277
Gigi Sayfan <the.gigi@gmail.com>
2023-01-13 00:42:56 -0800

    spaces -> tabs

--- pkg/client/common.go
+++ pkg/client/common.go
@@ -1,28 +1,28 @@
 package client
 
 import (
-    "k8s.io/client-go/rest"
-    "k8s.io/client-go/tools/clientcmd"
-    clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 )
 
 func getKubeConfig(kubeconfigPath string, kubeContext string) (config *rest.Config, err error) {
-    if kubeconfigPath == "" {
-        config, err = rest.InClusterConfig()
-        return
-    }
+	if kubeconfigPath == "" {
+		config, err = rest.InClusterConfig()
+		return
+	}
 
-    var conf *clientcmdapi.Config
-    conf, err = clientcmd.LoadFromFile(kubeconfigPath)
-    if err != nil {
-        return
-    }
+	var conf *clientcmdapi.Config
+	conf, err = clientcmd.LoadFromFile(kubeconfigPath)
+	if err != nil {
+		return
+	}
 
-    overrides := &clientcmd.ConfigOverrides{}
-    if kubeContext != "" {
-        overrides.CurrentContext = kubeContext
-    }
+	overrides := &clientcmd.ConfigOverrides{}
+	if kubeContext != "" {
+		overrides.CurrentContext = kubeContext
+	}
 
-    config, err = clientcmd.NewDefaultClientConfig(*conf, overrides).ClientConfig()
-    return
+	config, err = clientcmd.NewDefaultClientConfig(*conf, overrides).ClientConfig()
+	return
 }
